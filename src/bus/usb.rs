469eabbb5e32f390dc665b4f30ef3cc5563214d0
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-18 16:36:32 +0200

    feat; Implemented marker traits for channel identifying and channel condition

--- src/bus/usb.rs
+++ src/bus/usb.rs
@@ -1,4 +1,9 @@
+use crate::hw::channel_identifying::HasChannelIdentifying;
 use crate::pcan;
+
+use crate::hw::channel_condition::HasChannelCondition;
+use crate::channel::{Channel, ChannelCode};
+
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum UsbBus {
@@ -36,52 +41,61 @@ pub enum UsbBus {
     Usb16,
 }
 
-// impl From<UsbBus> for u16 {
-//     fn from(value: UsbBus) -> Self {
-//         let ret = match value {
-//             UsbBus::Usb1 => pcan::PCAN_USBBUS1,
-//             UsbBus::Usb2 => pcan::PCAN_USBBUS2,
-//             UsbBus::Usb3 => pcan::PCAN_USBBUS3,
-//             UsbBus::Usb4 => pcan::PCAN_USBBUS4,
-//             UsbBus::Usb5 => pcan::PCAN_USBBUS5,
-//             UsbBus::Usb6 => pcan::PCAN_USBBUS6,
-//             UsbBus::Usb7 => pcan::PCAN_USBBUS7,
-//             UsbBus::Usb8 => pcan::PCAN_USBBUS8,
-//             UsbBus::Usb9 => pcan::PCAN_USBBUS9,
-//             UsbBus::Usb10 => pcan::PCAN_USBBUS10,
-//             UsbBus::Usb11 => pcan::PCAN_USBBUS11,
-//             UsbBus::Usb12 => pcan::PCAN_USBBUS12,
-//             UsbBus::Usb13 => pcan::PCAN_USBBUS13,
-//             UsbBus::Usb14 => pcan::PCAN_USBBUS14,
-//             UsbBus::Usb15 => pcan::PCAN_USBBUS15,
-//             UsbBus::Usb16 => pcan::PCAN_USBBUS16,
-//         } as u16;
-//         ret
-//     }
-// }
+impl From<UsbBus> for ChannelCode {
+    fn from(value: UsbBus) -> Self {
+        let ret = match value {
+            UsbBus::Usb1 => pcan::PCAN_USBBUS1,
+            UsbBus::Usb2 => pcan::PCAN_USBBUS2,
+            UsbBus::Usb3 => pcan::PCAN_USBBUS3,
+            UsbBus::Usb4 => pcan::PCAN_USBBUS4,
+            UsbBus::Usb5 => pcan::PCAN_USBBUS5,
+            UsbBus::Usb6 => pcan::PCAN_USBBUS6,
+            UsbBus::Usb7 => pcan::PCAN_USBBUS7,
+            UsbBus::Usb8 => pcan::PCAN_USBBUS8,
+            UsbBus::Usb9 => pcan::PCAN_USBBUS9,
+            UsbBus::Usb10 => pcan::PCAN_USBBUS10,
+            UsbBus::Usb11 => pcan::PCAN_USBBUS11,
+            UsbBus::Usb12 => pcan::PCAN_USBBUS12,
+            UsbBus::Usb13 => pcan::PCAN_USBBUS13,
+            UsbBus::Usb14 => pcan::PCAN_USBBUS14,
+            UsbBus::Usb15 => pcan::PCAN_USBBUS15,
+            UsbBus::Usb16 => pcan::PCAN_USBBUS16,
+        } as u16;
+        ret.into()
+    }
+}
 
-// impl TryFrom<u16> for UsbBus {
-//     type Error = ();
+impl TryFrom<ChannelCode> for UsbBus {
+    type Error = ();
+    fn try_from(value: ChannelCode) -> Result<Self, Self::Error> {
+        let val = value.0 as u32;
+        match val {
+            pcan::PCAN_USBBUS1 => Ok(UsbBus::Usb1),
+            pcan::PCAN_USBBUS2 => Ok(UsbBus::Usb2),
+            pcan::PCAN_USBBUS3 => Ok(UsbBus::Usb3),
+            pcan::PCAN_USBBUS4 => Ok(UsbBus::Usb4),
+            pcan::PCAN_USBBUS5 => Ok(UsbBus::Usb5),
+            pcan::PCAN_USBBUS6 => Ok(UsbBus::Usb6),
+            pcan::PCAN_USBBUS7 => Ok(UsbBus::Usb7),
+            pcan::PCAN_USBBUS8 => Ok(UsbBus::Usb8),
+            pcan::PCAN_USBBUS9 => Ok(UsbBus::Usb9),
+            pcan::PCAN_USBBUS10 => Ok(UsbBus::Usb10),
+            pcan::PCAN_USBBUS11 => Ok(UsbBus::Usb11),
+            pcan::PCAN_USBBUS12 => Ok(UsbBus::Usb12),
+            pcan::PCAN_USBBUS13 => Ok(UsbBus::Usb13),
+            pcan::PCAN_USBBUS14 => Ok(UsbBus::Usb14),
+            pcan::PCAN_USBBUS15 => Ok(UsbBus::Usb15),
+            pcan::PCAN_USBBUS16 => Ok(UsbBus::Usb16),
+            _ => Err(())
+        }
+    }
+}
 
-//     fn try_from(value: u16) -> Result<Self, Self::Error> {
-//         match value as u32 {
-//             pcan::PCAN_USBBUS1 => Ok(UsbBus::USB1),
-//             pcan::PCAN_USBBUS2 => Ok(UsbBus::USB2),
-//             pcan::PCAN_USBBUS3 => Ok(UsbBus::USB3),
-//             pcan::PCAN_USBBUS4 => Ok(UsbBus::USB4),
-//             pcan::PCAN_USBBUS5 => Ok(UsbBus::USB5),
-//             pcan::PCAN_USBBUS6 => Ok(UsbBus::USB6),
-//             pcan::PCAN_USBBUS7 => Ok(UsbBus::USB7),
-//             pcan::PCAN_USBBUS8 => Ok(UsbBus::USB8),
-//             pcan::PCAN_USBBUS9 => Ok(UsbBus::USB9),
-//             pcan::PCAN_USBBUS10 => Ok(UsbBus::USB10),
-//             pcan::PCAN_USBBUS11 => Ok(UsbBus::USB11),
-//             pcan::PCAN_USBBUS12 => Ok(UsbBus::USB12),
-//             pcan::PCAN_USBBUS13 => Ok(UsbBus::USB13),
-//             pcan::PCAN_USBBUS14 => Ok(UsbBus::USB14),
-//             pcan::PCAN_USBBUS15 => Ok(UsbBus::USB15),
-//             pcan::PCAN_USBBUS16 => Ok(UsbBus::USB16),
-//             _ => Err(()),
-//         }
-//     }
-// }
+impl Channel for UsbBus {
+    fn channel(&self) -> ChannelCode {
+        self.clone().into()
+    }
+}
+
+impl HasChannelCondition for UsbBus {}
+impl HasChannelIdentifying for UsbBus {}
