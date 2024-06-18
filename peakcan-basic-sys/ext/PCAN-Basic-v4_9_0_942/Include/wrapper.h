c819f4dab3688f4de896c104578ec71c3a8de71f
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-18 09:00:36 +0200

    init: Added external dependency PCAN-Basic-v4_9_0_942

---
+++ peakcan-basic-sys/ext/PCAN-Basic-v4_9_0_942/Include/wrapper.h
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#ifndef DWORD
+#define DWORD uint32_t
+#endif
+
+#ifndef WORD
+#define WORD uint16_t
+#endif
+
+#ifndef UINT64
+#define UINT64 uint64_t
+#endif
+
+#ifndef LPSTR
+#define LPSTR char*
+#endif
+
+#ifndef BYTE
+#define BYTE uint8_t
+#endif
+
+#ifndef __stdcall
+#define __stdcall
+#endif 
+
+#include "PCANBasic.h"
