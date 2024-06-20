d1297ad47bef33e266b8c2314d6ed69a1b6a5dc3
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-20 07:01:42 +0200

    feat: Implemented public attached_channels_count function

--- src/hw/attached_channels.rs
+++ src/hw/attached_channels.rs
@@ -1,6 +1,23 @@
 use crate::pcan;
 use crate::error::{PcanError, PcanErrorCode, PcanOkError};
-use crate::channel::Channel;
 
 use std::ffi::c_void;
 
+pub fn attached_channel_count() -> Result<u32, PcanError> {
+    let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                pcan::PCAN_NONEBUS as u16,
+                pcan::PCAN_ATTACHED_CHANNELS_COUNT as u8,
+                data.as_mut_ptr() as *mut c_void,
+                4
+            )
+        };
+
+        let value = u32::from_le_bytes(data);
+        match PcanOkError::try_from(PcanErrorCode::from(code)) {
+            Ok(PcanOkError::Ok) => Ok(value),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+}
