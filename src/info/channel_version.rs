28bc3e0d93134af8dbf806e424baa37a03ab00ed
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-21 15:15:02 +0200

    feat: Implemented basic channel_version implementation

---
+++ src/info/channel_version.rs
@@ -0,0 +1,51 @@
+use crate::pcan;
+use crate::error::{PcanError, PcanErrorCode, PcanOkError};
+use crate::channel::Channel;
+
+use std::ffi::c_void;
+use std::str;
+
+
+// #[derive(Debug, PartialEq, Clone)]
+// pub struct Channel {
+//     data: [u8; pcan::MAX_LENGTH_HARDWARE_NAME as usize]
+// }
+
+// impl AsRef<str> for HwName {
+//     fn as_ref(&self) -> &str {
+//         str::from_utf8(&self.data).unwrap_or("").trim().trim_matches(char::from(0))
+//     }
+// }
+
+pub struct ChVersion {}
+
+pub(crate) trait HasChannelVersion {}
+
+pub trait ChannelVersion {
+    fn channel_version(&self) -> Result<ChVersion, PcanError>; 
+}
+
+impl<T: HasChannelVersion + Channel> ChannelVersion for T {
+    fn channel_version(&self) -> Result<ChVersion, PcanError> {
+        let mut data: [u8; 33] = [b'\0'; pcan::MAX_LENGTH_HARDWARE_NAME as usize];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel().into(),
+                pcan::PCAN_HARDWARE_NAME as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32
+            )
+        };
+
+        match PcanOkError::try_from(PcanErrorCode::from(code)) {
+            Ok(PcanOkError::Ok) => {
+                match data.is_ascii() {
+                    false => Err(PcanError::Unknown),
+                    true => Ok(ChVersion {})
+                }
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
