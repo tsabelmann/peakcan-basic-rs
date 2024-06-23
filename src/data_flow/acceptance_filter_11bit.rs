617d1c44b877a3c06e38c8644269d0cd911d9e3b
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-23 13:40:01 +0200

    feat: Implemented acceptance filter 11bit functionality

---
+++ src/data_flow/acceptance_filter_11bit.rs
@@ -0,0 +1,64 @@
+use crate::pcan;
+use crate::error::{PcanError, PcanErrorCode, PcanOkError};
+use crate::channel::Channel;
+
+use std::ffi::c_void;
+
+pub(crate) trait HasAcceptanceFilter11bit {}
+pub(crate) trait HasSetAcceptanceFilter11bit {}
+
+pub trait AcceptanceFilter11bit {
+    fn acceptance_filter_11bit(&self) -> Result<(u32, u32), PcanError>;
+}
+
+pub trait SetAcceptanceFilter11bit {
+    fn set_acceptance_filter_11bit(&self, code: u32, mask: u32) -> Result<(), PcanError>; 
+}
+
+impl<T: HasAcceptanceFilter11bit + Channel> AcceptanceFilter11bit for T {
+    fn acceptance_filter_11bit(&self) -> Result<(u32, u32), PcanError> {
+        let mut data = [0u8; 8];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel().into(),
+                pcan::PCAN_ACCEPTANCE_FILTER_11BIT as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32
+            )
+        };
+
+        let value = u64::from_le_bytes(data);
+        match PcanOkError::try_from(PcanErrorCode::from(code)) {
+            Ok(PcanOkError::Ok) => {
+                let code = ((value >> 32) & 0xFF_FF_FF_FF) as u32;
+                let mask = ((value >> 0) & 0xFF_FF_FF_FF) as u32;
+                Ok((code, mask))
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
+impl<T: HasSetAcceptanceFilter11bit + Channel> SetAcceptanceFilter11bit for T {
+    fn set_acceptance_filter_11bit(&self, code: u32, mask: u32) -> Result<(), PcanError> {
+        let value = ((code as u64) << 32) | ((mask as u64) << 0);
+        let mut data = value.to_le_bytes();
+        println!("{:?}", data);
+
+        let code = unsafe {
+            pcan::CAN_SetValue(
+                self.channel().into(),
+                pcan::PCAN_ACCEPTANCE_FILTER_11BIT as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32
+            )
+        };
+
+        match PcanOkError::try_from(PcanErrorCode::from(code)) {
+            Ok(PcanOkError::Ok) => Ok(()),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
