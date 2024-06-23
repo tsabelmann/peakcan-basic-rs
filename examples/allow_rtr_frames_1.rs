72ca02567469cb1078324c93bd62493511bbbd88
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-23 12:54:32 +0200

    init: Added allow_rtr_frames_1.rs

---
+++ examples/allow_rtr_frames_1.rs
@@ -0,0 +1,20 @@
+use peakcan_basic::{bus::usb::UsbBus, data_flow::allow_rtr_frames::{AllowRtrFrames, SetAllowRtrFrames}, socket::usb::UsbSocket};
+
+fn main() {
+    let socket = UsbSocket::open(UsbBus::Usb1, peakcan_basic::baudrate::Baudrate::Baud500K).expect("Could not open USB1");
+
+    let result = socket.allow_rtr_frames();
+    println!("Allow-RTR-Frames-Status: {:?}", result);
+
+    let result = socket.set_allow_rtr_frames(false);
+    println!("Set-Allow-RTR-Frames-Status: {:?}", result);
+
+    let result = socket.allow_rtr_frames();
+    println!("Allow-RTR-Frames-Status: {:?}", result);
+
+    let result = socket.set_allow_rtr_frames(true);
+    println!("Set-Allow-RTR-Frames-Status: {:?}", result);
+
+    let result = socket.allow_rtr_frames();
+    println!("Allow--Frames-Status: {:?}", result);
+}
