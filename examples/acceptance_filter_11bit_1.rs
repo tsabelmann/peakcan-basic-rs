3dbb1b77c99cf410e19221adb47c0a57c9e9e455
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-23 13:40:59 +0200

    init: Added acceptance_filter_11bit_1.rs

---
+++ examples/acceptance_filter_11bit_1.rs
@@ -0,0 +1,14 @@
+use peakcan_basic::{bus::usb::UsbBus, data_flow::{acceptance_filter_11bit::{AcceptanceFilter11bit, SetAcceptanceFilter11bit}, allow_status_frames::{AllowStatusFrames, SetAllowStatusFrames}}, socket::usb::UsbSocket};
+
+fn main() {
+    let socket = UsbSocket::open(UsbBus::Usb1, peakcan_basic::baudrate::Baudrate::Baud500K).expect("Could not open USB1");
+
+    let result = socket.acceptance_filter_11bit();
+    println!("Acceptance-Filter-11bit: {:?}", result);
+
+    let result = socket.set_acceptance_filter_11bit(0x123, 0x07_FF);
+    println!("Set-Acceptance-Filter-11bit: {:?}", result);
+
+    let result = socket.acceptance_filter_11bit();
+    println!("Acceptance-Filter-11bit: {:?}", result);
+}
