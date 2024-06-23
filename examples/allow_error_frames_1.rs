1f6b043567c8b60cc3d2324310188b5a8c44e268
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-23 13:17:47 +0200

    style: Changed spelling

--- examples/allow_error_frames_1.rs
+++ examples/allow_error_frames_1.rs
@@ -4,17 +4,17 @@ fn main() {
     let socket = UsbSocket::open(UsbBus::Usb1, peakcan_basic::baudrate::Baudrate::Baud500K).expect("Could not open USB1");
 
     let result = socket.allow_error_frames();
-    println!("Allow-ERROR-Frames-Status: {:?}", result);
+    println!("Allow-Error-Frames-Status: {:?}", result);
 
     let result = socket.set_allow_error_frames(true);
-    println!("Set-Allow-ERROR-Frames-Status: {:?}", result);
+    println!("Set-Allow-Error-Frames-Status: {:?}", result);
 
     let result = socket.allow_error_frames();
-    println!("Allow-ERROR-Frames-Status: {:?}", result);
+    println!("Allow-Error-Frames-Status: {:?}", result);
 
     let result = socket.set_allow_error_frames(false);
-    println!("Set-Allow-ERROR-Frames-Status: {:?}", result);
+    println!("Set-Allow-Error-Frames-Status: {:?}", result);
 
     let result = socket.allow_error_frames();
-    println!("Allow-ERROR-Frames-Status: {:?}", result);
+    println!("Allow-Error-Frames-Status: {:?}", result);
 }
