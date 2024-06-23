11c28901659775227b26d40434bee57148161343
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-23 13:08:33 +0200

    init: Added allow_error_frames_1.rs

---
+++ examples/allow_error_frames_1.rs
@@ -0,0 +1,20 @@
+use peakcan_basic::{bus::usb::UsbBus, data_flow::allow_error_frames::{AllowErrorFrames, SetAllowErrorFrames}, socket::usb::UsbSocket};
+
+fn main() {
+    let socket = UsbSocket::open(UsbBus::Usb1, peakcan_basic::baudrate::Baudrate::Baud500K).expect("Could not open USB1");
+
+    let result = socket.allow_error_frames();
+    println!("Allow-ERROR-Frames-Status: {:?}", result);
+
+    let result = socket.set_allow_error_frames(true);
+    println!("Set-Allow-ERROR-Frames-Status: {:?}", result);
+
+    let result = socket.allow_error_frames();
+    println!("Allow-ERROR-Frames-Status: {:?}", result);
+
+    let result = socket.set_allow_error_frames(false);
+    println!("Set-Allow-ERROR-Frames-Status: {:?}", result);
+
+    let result = socket.allow_error_frames();
+    println!("Allow-ERROR-Frames-Status: {:?}", result);
+}
