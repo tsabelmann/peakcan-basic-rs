31b4ac55009ccfced5c518b43889af8428643b39
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-23 12:50:47 +0200

    feat: Implemented allow RTR frames funtionality

---
+++ src/data_flow/allow_rtr_frames.rs
@@ -0,0 +1,64 @@
+use crate::pcan;
+use crate::error::{PcanError, PcanErrorCode, PcanOkError};
+use crate::channel::Channel;
+
+use std::ffi::c_void;
+
+pub(crate) trait HasAllowRtrFrames {}
+pub(crate) trait HasSetAllowRtrFrames {}
+
+pub trait AllowRtrFrames {
+    fn allow_rtr_frames(&self) -> Result<bool, PcanError>;
+}
+
+pub trait SetAllowRtrFrames {
+    fn set_allow_rtr_frames(&self, value: bool) -> Result<(), PcanError>; 
+}
+
+impl<T: HasAllowRtrFrames + Channel> AllowRtrFrames for T {
+    fn allow_rtr_frames(&self) -> Result<bool, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel().into(),
+                pcan::PCAN_ALLOW_RTR_FRAMES as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32
+            )
+        };
+
+        let value = u32::from_le_bytes(data);
+        match PcanOkError::try_from(PcanErrorCode::from(code)) {
+            Ok(PcanOkError::Ok) => match value {
+                pcan::PCAN_PARAMETER_OFF => Ok(false),
+                pcan::PCAN_PARAMETER_ON => Ok(true),
+                _ => Err(PcanError::Unknown)
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
+impl<T: HasSetAllowRtrFrames + Channel> SetAllowRtrFrames for T {
+    fn set_allow_rtr_frames(&self, value: bool) -> Result<(), PcanError> {
+        let mut data = match value {
+            true => pcan::PCAN_PARAMETER_ON,
+            false => pcan::PCAN_PARAMETER_OFF,
+        }.to_le_bytes();
+        let code = unsafe {
+            pcan::CAN_SetValue(
+                self.channel().into(),
+                pcan::PCAN_ALLOW_RTR_FRAMES as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32
+            )
+        };
+
+        match PcanOkError::try_from(PcanErrorCode::from(code)) {
+            Ok(PcanOkError::Ok) => Ok(()),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
