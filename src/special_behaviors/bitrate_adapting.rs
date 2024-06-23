b5edb9a15e5964c7469b4485eaed17cb9a255c2e
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-23 11:32:31 +0200

    feat: Implemented bitrate adapting functionality

---
+++ src/special_behaviors/bitrate_adapting.rs
@@ -0,0 +1,66 @@
+use crate::pcan;
+use crate::error::{PcanError, PcanErrorCode, PcanOkError};
+use crate::channel::Channel;
+
+use std::ffi::c_void;
+
+pub(crate) trait HasBitrateAdapting {}
+pub(crate) trait HasSetBitrateAdapting {}
+
+pub trait BitrateAdapting {
+    fn bitrate_adapting(&self) -> Result<bool, PcanError>;
+}
+
+pub trait SetBitrateAdapting {
+    fn set_bitrate_adapting(&self, value: bool) -> Result<(), PcanError>; 
+}
+
+impl<T: HasBitrateAdapting + Channel> BitrateAdapting for T {
+    fn bitrate_adapting(&self) -> Result<bool, PcanError> {
+        let mut data = [0u8; 4];
+        let code = unsafe {
+            pcan::CAN_GetValue(
+                self.channel().into(),
+                pcan::PCAN_BITRATE_ADAPTING as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32
+            )
+        };
+
+        let value = u32::from_le_bytes(data);
+        match PcanOkError::try_from(PcanErrorCode::from(code)) {
+            Ok(PcanOkError::Ok) => {
+                match value {
+                    pcan::PCAN_PARAMETER_OFF => Ok(false),
+                    pcan::PCAN_PARAMETER_ON => Ok(true),
+                    _ => Err(PcanError::Unknown)
+                }
+            },
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
+
+impl<T: HasSetBitrateAdapting + Channel> SetBitrateAdapting for T {
+    fn set_bitrate_adapting(&self, value: bool) -> Result<(), PcanError> {
+        let mut data = match value {
+            true => pcan::PCAN_PARAMETER_ON,
+            false => pcan::PCAN_PARAMETER_OFF,
+        }.to_le_bytes();
+        let code = unsafe {
+            pcan::CAN_SetValue(
+                self.channel().into(),
+                pcan::PCAN_BITRATE_ADAPTING as u8,
+                data.as_mut_ptr() as *mut c_void,
+                data.len() as u32
+            )
+        };
+
+        match PcanOkError::try_from(PcanErrorCode::from(code)) {
+            Ok(PcanOkError::Ok) => Ok(()),
+            Ok(PcanOkError::Err(err)) => Err(err),
+            Err(_) => Err(PcanError::Unknown),
+        }
+    }
+}
