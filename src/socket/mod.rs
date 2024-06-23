91857308eb3f761e43574b389d01d32bf8f5ab76
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-23 09:26:27 +0200

    feat: Added trait Socket

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -1,2 +1,14 @@
+use crate::{error::PcanError, frame::CanFrame};
+use crate::timestamp::Timestamp;
+
 pub mod fd;
 pub mod usb;
+
+pub trait Socket {
+    fn send(&self, frame: CanFrame) -> Result<(), PcanError>;
+    fn recv(&self) -> Result<(CanFrame, Timestamp), PcanError>; 
+    fn recv_frame(&self) -> Result<CanFrame, PcanError> {
+        let (frame, _) = self.recv()?;
+        Ok(frame)
+    }
+}
