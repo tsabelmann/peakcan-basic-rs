addca458ae6c346a6439fee93f6835096261d762
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-24 08:47:50 +0200

    feat: Implemented log_status and set_log_status functions

--- src/logging.rs
+++ src/logging.rs
@@ -50,3 +50,48 @@ pub fn set_log_location<P: AsRef<Path>>(path: P) -> Result<(), PcanError> {
         Err(_) => Err(PcanError::Unknown),
     }
 }
+
+pub fn log_status() -> Result<bool, PcanError> {
+    let mut data = [0u8; 4];
+    let code = unsafe {
+        pcan::CAN_GetValue(
+            pcan::PCAN_NONEBUS as u16,
+            pcan::PCAN_LOG_STATUS as u8,
+            data.as_mut_ptr() as *mut c_void,
+            data.len() as u32
+        )
+    };
+
+    let value = u32::from_le_bytes(data);
+    match PcanOkError::try_from(PcanErrorCode::from(code)) {
+        Ok(PcanOkError::Ok) => match value {
+            pcan::PCAN_PARAMETER_OFF => Ok(false),
+            pcan::PCAN_PARAMETER_ON => Ok(true),
+            _ => Err(PcanError::Unknown)
+        },
+        Ok(PcanOkError::Err(err)) => Err(err),
+        Err(_) => Err(PcanError::Unknown),
+    }
+}
+
+pub fn set_log_status(value: bool) -> Result<(), PcanError> {
+    let data = match value {
+        false => pcan::PCAN_PARAMETER_OFF,
+        true => pcan::PCAN_PARAMETER_ON
+    }.to_le_bytes();
+
+    let code = unsafe {
+        pcan::CAN_SetValue(
+            pcan::PCAN_NONEBUS as u16,
+            pcan::PCAN_LOG_STATUS as u8,
+            data.as_ptr() as *mut c_void,
+            data.len() as u32,
+        )
+    };
+
+    match PcanOkError::try_from(PcanErrorCode::from(code)) {
+        Ok(PcanOkError::Ok) => Ok(()),
+        Ok(PcanOkError::Err(err)) => Err(err),
+        Err(_) => Err(PcanError::Unknown),
+    }
+}
