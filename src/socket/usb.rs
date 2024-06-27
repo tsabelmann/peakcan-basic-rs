1f1f22acbf7a5a24c99e918bbd8d5abc5bfd3968
tsabelmann <tim.sabelmann@hotmail.de>
2024-06-27 10:07:39 +0200

    feat: Implemented marker trait for set device id functionality

--- src/socket/usb.rs
+++ src/socket/usb.rs
@@ -8,6 +8,7 @@ use crate::data_flow::allow_status_frames::{HasAllowStatusFrames, HasSetAllowSta
 use crate::data_flow::message_filter::{HasMessageFilter, HasSetMessageFilter};
 use crate::data_flow::receive_status::{HasReceiveStatus, HasSetReceiveStatus};
 use crate::frame::CanFrame;
+use crate::hw::device_id::HasSetDeviceId;
 use crate::info::bitrate_info::HasBitrateInfo;
 use crate::info::busspeed_data::HasBusSpeedData;
 use crate::info::busspeed_nominal::HasBusSpeedNominal;
@@ -65,6 +66,10 @@ impl Channel for UsbSocket {
         }
     }
 }
+
+/* Marker trait implementations - hardware */
+
+impl HasSetDeviceId for UsbSocket {}
 
 /* Marker traits */
 
