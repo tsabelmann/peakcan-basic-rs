1c46bf56543aca8102c63f20dd2dc9b35dacdf61
tsabelmann <tim.sabelmann@hotmail.de>
2024-07-01 15:15:55 +0200

    feat: Added re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -21,5 +21,11 @@ pub use baudrate::{Baudrate, Btr0Btr1, Btr0Btr1Code};
 /// Re-export of bus module.
 pub use bus::{dng::DngBus, isa::IsaBus, lan::LanBus, pcc::PccBus, pci::PciBus, usb::UsbBus};
 
+/// Re-export of channel module.
+pub use channel::{Channel, ChannelCode};
+
+/// Re-export of error module.
+pub use error::{PcanError, PcanOkError, PcanErrorCode};
+
 /// Re-export of frame module.
 pub use frame::{CanFrame, CanFdFrame, CanFrameBuilder};
