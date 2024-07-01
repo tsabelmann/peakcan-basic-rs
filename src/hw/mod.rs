2739b9692340eee6976f61a9940b3312fe082f56
tsabelmann <tim.sabelmann@hotmail.de>
2024-07-01 15:04:27 +0200

    feat: Added re-exports

--- src/hw/mod.rs
+++ src/hw/mod.rs
@@ -5,3 +5,11 @@ pub mod hardware_name;
 pub mod controller_number;
 pub mod ip_address;
 pub mod device_part_number;
+
+pub use channel_condition::{ChannelCondition, ChannelConditionStatus, ChannelConditionStatusCode};
+pub use channel_identifying::ChannelIdentifying;
+pub use device_id::{DeviceId, SetDeviceId};
+pub use hardware_name::{HwName, HardwareName};
+pub use controller_number::{ControllerNumber, SetControllerNumber};
+pub use ip_address::IpAddress;
+pub use device_part_number::{DevPartNumber, DevicePartNumber};
