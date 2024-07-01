2f991dcb1729d4a962abbeb68138cbd0a34fa04d
tsabelmann <tim.sabelmann@hotmail.de>
2024-07-01 15:09:33 +0200

    feat: Added re-exports

--- src/trace/mod.rs
+++ src/trace/mod.rs
@@ -2,3 +2,8 @@ pub mod trace_location;
 pub mod trace_status;
 pub mod trace_size;
 pub mod trace_configure;
+
+pub use trace_location::{TraceLocation, SetTraceLocation};
+pub use trace_status::{TraceStatus, SetTraceStatus};
+pub use trace_size::{TraceSize, SetTraceSize};
+pub use trace_configure::{TraceConfigure, SetTraceConfigure};
